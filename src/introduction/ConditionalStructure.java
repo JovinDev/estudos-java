5695669f5669a6787e808b750ef71ae5030866ba
JovinDev <joo.vinicius2005@gmail.com>
2024-06-13 16:14:12 -0300

    Update ConditionalStructure

---
+++ src/introduction/ConditionalStructure.java
@@ -0,0 +1,26 @@
+
+package introduction;
+import java.util.Scanner;
+
+public class ConditionalStructure {
+    public static void main (String[] args){
+        // Create a Scanner object to read user input
+        try ( Scanner scanner = new Scanner(System.in)) {
+
+            // Prompt the user to enter a number
+            System.out.print("Enter a number");
+            int number = scanner.nextInt();
+            
+            // Conditional structure if-else to check if the number is positive, negative, or zero
+            if (number > 0) {
+                System.out.print("This number is positive");}
+            else if (number < 0) {
+                System.out.print("This number is negative");}
+            else {
+                System.out.println("This number is zero");
+            }
+        }
+
+
+    }
+}
