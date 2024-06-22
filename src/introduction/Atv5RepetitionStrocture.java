a971133ae645b4daeb89f6fe459c4488700bfdd3
JovinDev <joo.vinicius2005@gmail.com>
2024-06-22 05:20:15 -0300

    update atv5

--- src/introduction/Atv5RepetitionStrocture.java
+++ src/introduction/Atv5RepetitionStrocture.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 
 public class Atv5RepetitionStrocture {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Get the value from the user 
+            System.out.println("Type the limit value: ");
+            int limit = scanner.nextInt();
 
-        // Get the value from the user 
-        System.out.println("Type the limit value: ");
-        int limit = scanner.nextInt();
+            // Using the for loop
+            System.out.println("using for");
+            for (int i = 1; i <= limit; i++) {
+                System.out.println(i + "");
+            }
 
-        // Using the for loop
-        System.out.println("using for");
-        for (int i = 1; i <= limit; i++) {
-            System.out.println(i + "");
+            // to skip a line
+            System.out.println();
+
+            
+
+            scanner.close();
         }
-
-        // to skip a line
-        System.out.println();
-
-        
-
-        scanner.close();
     }
 
 }
