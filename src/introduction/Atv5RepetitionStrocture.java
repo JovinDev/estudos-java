aae7e4ed5e3d7a7464d82e9c918e9d595f334416
JovinDev <joo.vinicius2005@gmail.com>
2024-06-22 05:16:32 -0300

    update atv5

--- src/introduction/Atv5RepetitionStrocture.java
+++ src/introduction/Atv5RepetitionStrocture.java
@@ -1,5 +1,26 @@
 package introduction;
+import java.util.Scanner;
 
 public class Atv5RepetitionStrocture {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the value from the user 
+        System.out.println("Type the limit value: ");
+        int limit = scanner.nextInt();
+
+        // Using the for loop
+        System.out.println("using for");
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i + "");
+        }
+
+        // to skip a line
+        System.out.println();
+
+        
+
+        scanner.close();
+    }
+
 }
